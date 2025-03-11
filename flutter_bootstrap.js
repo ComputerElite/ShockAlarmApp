9f0b80053cd7159f9304d2b40fc7e88531bc8e16
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-03-11 19:08:25 +0000

    Deploying to gh-pages from @ ComputerElite/ShockAlarmApp@246196d42bacebb22b4de26da2ebedb495288715 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1111843333"
+    serviceWorkerVersion: "287375515"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "2806d119770484b438526f2704bc2685",
 "manifest.json": "0c8213e3b89f788c150e51fda6e7f89b",
 "favicon.png": "a3a82b98e5be892d2affe92ebd2f6a56",
-"flutter_bootstrap.js": "29889b106ed75932e182ccc9b6d682aa"};
+"flutter_bootstrap.js": "a499826b856cf56409bae13d5dd2fbe3"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
