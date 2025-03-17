a00656e2f4e1b2eacf74ee3e567af83915bf1d03
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-03-17 20:33:56 +0000

    Deploying to gh-pages from @ ComputerElite/ShockAlarmApp@97dadbe3dc4c6ac39e26dc19e6933d06b56f6445 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3113804456"
+    serviceWorkerVersion: "2563252315"
   }
 });
