b6ee2b0fa57b561e1c21406ff9ca9a8954490608
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-05-04 10:24:11 +0000

    Deploying to gh-pages from @ ComputerElite/ShockAlarmApp@cfed34099a1869ba7367f97a830aa4cfa761170b 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {"assets/AssetManifest.json": "d88d9a534eacb878f982d2a5926ac11
 "assets/NOTICES": "8f5174dabd01c9871429de420d50decf",
 "assets/packages/wakelock_plus/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
 "manifest.json": "0c8213e3b89f788c150e51fda6e7f89b",
-"version.json": "4d67dc6d99c366f9e6fcfe8218484918",
+"version.json": "a45ae47c5e8586b84d033d31f0b9dccb",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.json": "d88d9a534eacb878f982d2a5926ac11
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "index.html": "d24d21b1c2a092cbac4af5da437599ec",
 "/": "d24d21b1c2a092cbac4af5da437599ec",
-"flutter_bootstrap.js": "96beffeb3a11eb4df1618edd5cd67b93",
+"flutter_bootstrap.js": "049677dd45f53fafee0d7dbb6fc8c7fc",
 "icons/Icon-any.svg": "9063863f7365492c863068f6d21180af",
 "icons/Icon-192.png": "2806d119770484b438526f2704bc2685",
 "icons/icon-idk.webp": "7f45b099976f8d89113990d12cf0f597",
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.json": "d88d9a534eacb878f982d2a5926ac11
 "icons/Icon-512.png": "fffd22a56215891c889991eccd8a7859",
 "icons/Icon-512-transparent.png": "efb797a137b65b50653a2a4907a00692",
 "favicon.png": "a3a82b98e5be892d2affe92ebd2f6a56",
-"main.dart.js": "cedbb52592c26547250e243651f67676"};
+"main.dart.js": "ea1f0616f0c491fb00d67ad6280366b5"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
